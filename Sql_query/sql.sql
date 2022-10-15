30bf181994d37a4a89825f97850ddbdf3998db2b
s-babaei <sara.babaei.8941@gmail.com>
2022-10-15 18:23:03 +0330

    5rd Commit

--- Sql_query/sql.sql
+++ Sql_query/sql.sql
@@ -2,7 +2,7 @@
 select student.id, count(student.id) as courseCount,person_name from student
  inner join st_co onstudent.id=st_co.st_id
 group by st_co.st_id
-having courseCount>2;
+having courseCount>10;
 -- 2-
 select * from course where not exists(select * from st_co where st_co.co_id=course.id);
 
